5eafbf3a848f97fbb02361e6845b25de25ca29b7
GersonESS <gebhsantos@gmail.com>
2025-01-24 19:20:25 -0300

    Update app.js

--- 01 - Instalando/src/app.js
+++ 01 - Instalando/src/app.js
@@ -3,7 +3,11 @@ const express = require("express");
 const app = express();
 app.get("/", (req, res) => {
   res.send("Hello World!");
-});     
-app.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000");
+}); 
+
+
+app.listen(8080, () => {
+  console.log('Rodando app listening at http://localhost:8080');
 });
+
+
